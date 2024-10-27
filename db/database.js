267c6ba8538bbc7153335dd883a9506d89a0f968
Crlsljndr <carlosalejandro-@hotmail.cl>
2024-10-27 16:39:39 -0300

    arreglos de dotenv

--- db/database.js
+++ db/database.js
@@ -1,12 +1,15 @@
 const { MongoClient } = require('mongodb');
+require('dotenv').config();
 
-const uri = "mongodb+srv://user_01:user_01@cluster0.ts8cixu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.USERNAME_DB}:${process.env.PASSWORD_DB}@cluster0.ts8cixu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
 const client = new MongoClient(uri);
 
 async function connectDB() {
     try {
         await client.connect();
         const db = client.db("DBD");
+        console.log("Conexión exitosa a MongoDB Atlas");
         return db;
     } catch (error) {
         console.error("Error al conectar a MongoDB Atlas", error);
